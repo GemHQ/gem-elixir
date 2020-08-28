23e35ad56d14910ae222a935fc06749a756fbd90
Chris Almeida <chris@gem.co>
2020-08-28 17:03:15 -0600

    add error map to error struct

--- lib/error.ex
+++ lib/error.ex
@@ -1,7 +1,8 @@
 defmodule Gem.APIError do
   defstruct(
-    code: "ERR_INTERNAL_SERVER",
+    error: "ERR_INTERNAL_SERVER",
     description: "An unknown error occurred.",
-    status: 500
+    status: 500,
+    error_map: nil
   )
 end
